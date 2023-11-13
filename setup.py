bcf1a067ecae434b5f226023f22cbab30a6af6f5
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-11-13 18:23:41 +0300

    build: rework setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-
-setup(name='Openapi2jsonschema',
-      version='1.0',
-      description='OpenAPI to JSON Schemas converter',
-      author='Yann Hamon',
-      author_email='yann@mandragor.org',
-      url='http://github.com/yannh/openapi2jsonschema',
-      packages=['openapi2jsonschema'],
-      )
+setup(
+    name="openapi2jsonschema",
+    version="0.9.0",
+    description="OpenAPI to JSON schemas converter",
+    long_description="Converts OpenAPI definitions into JSON schemas for all types in the API",
+    author="Gareth Rushgrove",
+    author_email="gareth@morethanseven.net",
+    maintainer="Stanislav Asunkin",
+    license="Apache-2.0",
+    keywords=["openapi", "jsonschema"],
+    url="https://github.com/stasjok/openapi2jsonschema",
+    packages=["openapi2jsonschema"],
+    install_requires=[
+        "click >= 7.0",
+        "PyYAML >= 5.1",
+        "jsonref >= 0.2.0",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "openapi2jsonschema = openapi2jsonschema.command:default",
+        ]
+    },
+)
