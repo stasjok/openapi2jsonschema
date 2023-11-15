stasjok/openapi2jsonschema

commit 20616ac9dc2f9d29a234e8205e5e127fcf3cf2b3

remove the lower to make kind and title case match : this make file name and ref file name match

openapi2jsonschema/command.py
@@ -133,7 +133,7 @@ def default(output, schema, prefix, stand_alone, expanded, kubernetes, strict):
         components = data["components"]["schemas"]
 
     for title in components:
-        kind = title.split(".")[-1].lower()
+        kind = title.split(".")[-1]
         if kubernetes:
             try:
                 group = title.split(".")[-3].lower()

commit ca9557f243f294076500d325cfc5ffadeeb1dd4e

fix: fix unsupported kinds check regression introduced by 20616ac

openapi2jsonschema/command.py
@@ -174,7 +174,7 @@ def default(output, schema, prefix, stand_alone, expanded, kubernetes, strict):
             if (
                 kubernetes
                 and stand_alone
-                and kind
+                and kind.lower()
                 in [
                     "jsonschemaprops",
                     "jsonschemapropsorarray",